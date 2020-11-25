2427dda0f6eac96e6dc557d1a08689e3d1877d28
Subhasish10 <palsubhasish6@gmail.com>
2020-11-26 02:12:35 +0530

    Delete unnecssary

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client Site js loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msgOne = document.querySelector('#msg-1')
